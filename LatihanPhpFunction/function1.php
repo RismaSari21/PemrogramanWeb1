ebb6850f086707414e7f243180a0cc835b131e84
RismaSari21 <rissmasari9@gmail.com>
2026-06-18 10:10:34 +0700

    update

---
+++ LatihanPhpFunction/function1.php
@@ -0,0 +1,12 @@
+<?php
+
+// Membuat Function
+function sapaPengguna() {
+    echo "Selamat datang di praktikum PHP Function!<br>";
+}
+
+// Memanggil Function
+sapaPengguna();
+sapaPengguna();
+
+?>
